73684591b4634ea8a985010b9146a92d59f1583a
WanderJunio1226 <wanderportfolio@gmail.com>
2023-01-23 17:59:29 -0300

    correcao de arquivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="limite topo">
+        <p className="azuldestaque ptopo">React JS</p>
       </header>
+      <main className="limite">
+        <section className="banner cinzadestaque">
+          <div className=" ">
+            <h1 className="azuldestaque">React JS</h1>
+            <p className="brancodestaque">Biblioteca JavaScript para criar SPAs</p>
+            <nav className="linkbanner" >
+              <a className="linkdestaque" href="https://reactjs.org/">Site Oficial</a>
+            </nav>
+          </div>
+        </section>
+        <section className="">
+            <h2>Requisitos</h2>
+            <p>Veja abaixo os requisitos mínimos para aprender React JS</p>
+          <div className="limitasecao">
+            <div className="boxlinguagens cinzadestaque">
+              <h3 className="golddestaque">React JS</h3> <p className="brancodestaque">HTML Aprender a estruturar os elementos da
+              página com HTML</p> 
+            </div>
+            <div className="boxlinguagens cinzadestaque">
+              <h3 className="golddestaque">CSS</h3> <p className="brancodestaque"> Aprender a estilizar os elementos da página com CSS{" "}</p>
+            </div>
+            <div className="boxlinguagens cinzadestaque">
+              <h3  className="golddestaque">JavaScript</h3> <p className="brancodestaque"> Aprender a linguagem utilizada pelo React JS,
+              o JavaScript</p>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="limite foooter">
+        <p>Curso de introdução de React JS</p>
+      </footer>
     </div>
   );
 }
